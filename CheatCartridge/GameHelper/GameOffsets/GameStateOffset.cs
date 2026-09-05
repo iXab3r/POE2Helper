39292c32ec91993d591e87cdd69c43c61942e1cc
Xab3r <>
2026-09-05 11:35:51 +0200

    Update GameStates and area offsets for current PoE2 client

--- CheatCartridge/GameHelper/GameOffsets/GameStateOffset.cs
+++ CheatCartridge/GameHelper/GameOffsets/GameStateOffset.cs
@@ -5,6 +5,8 @@ namespace CheatCartridge.GameHelper.GameOffsets;
 [StructLayout(LayoutKind.Explicit, Pack = 1)]
 public struct GameStateOffset
 {
-    [FieldOffset(0x08)] public StdVector CurrentStatePtr; // Used in RemoteObject -> CurrentState
-    [FieldOffset(0x48)] public GameStateBuffer States;
+    // 0x08 -> 0x10 (+8)
+    [FieldOffset(0x10)] public StdVector CurrentStatePtr; // Used in RemoteObject -> CurrentState
+    // 0x48 -> 0x50 (+8)
+    [FieldOffset(0x50)] public GameStateBuffer States;
 }
--- CheatCartridge/GameHelper/GameOffsets/States/InGameState/AreaInstanceOffsets.cs
+++ CheatCartridge/GameHelper/GameOffsets/States/InGameState/AreaInstanceOffsets.cs
@@ -12,21 +12,21 @@ public struct AreaInstanceOffsets
     
     /// <summary>
     /// == Monster Level, Clearfell = 2
-    /// 0xB4 -> 0xBC (+8) -> 0xC4 (+8) 
+    /// 0xB4 -> 0xBC (+8) -> 0xC4 (+8) -> 0xBC (-8)
     /// </summary>
-    [FieldOffset(0x0C4)] public byte CurrentAreaLevel;
+    [FieldOffset(0x0BC)] public byte CurrentAreaLevel;
     
     /// <summary>
     /// Usually has quite high entrhtropy, e.g. 1494246552
-    /// 0xF4 -> 0xFC (+8) -> 0x104 (+8) => 0x11C (+0x18)
+    /// 0xF4 -> 0xFC (+8) -> 0x104 (+8) => 0x11C (+0x18) -> 0x114 (-8)
     /// </summary>
-    [FieldOffset(0x11C)] public uint CurrentAreaHash;
+    [FieldOffset(0x114)] public uint CurrentAreaHash;
     
     /// <summary>
-    /// Before this ptr there are 28 zeroes
-    /// 0x588 -> 0x5A0 (+0x18) -> 0x5A8 (+8)
+    /// The ServerData pointer is immediately before this vector at +0x5B0.
+    /// 0x588 -> 0x5A0 (+0x18) -> 0x5A8 (+8) -> 0x5B8 (+0x10)
     /// </summary>
-    [FieldOffset(0x5A8)] public StdVector LocalPlayers;
+    [FieldOffset(0x5B8)] public StdVector LocalPlayers;
     
     /// <summary>
     /// Some oscillating value, 0.07 - 0.30, ping?
@@ -42,7 +42,7 @@ public struct AreaInstanceOffsets
     
     /// <summary>
     /// In Clearfell at tp ~30-50
-    /// 0xB58 -> 0xB60 (+8) -> 0xB68 (+8) -> 0x6E8 (-0x480)
+    /// 0xB58 -> 0xB60 (+8) -> 0xB68 (+8) -> 0x6E8 (-0x480) -> 0x6F8 (+0x10)
     /// </summary>
-    [FieldOffset(0x6E8)] public uint EntitiesCount;
+    [FieldOffset(0x6F8)] public uint EntitiesCount;
 }
